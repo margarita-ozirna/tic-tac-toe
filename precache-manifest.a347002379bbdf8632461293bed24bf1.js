c3691d34625e361599bc5afd27b87a7bcacd2760
OzirnaRita <ritkapuh1@gmail.com>
2019-06-13 12:25:42 +0300

    Updates

---
+++ precache-manifest.a347002379bbdf8632461293bed24bf1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f552f8ba2ad6b36e0e4cef35564ea807",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "a43f3440d3817e3ad002",
+    "url": "/tic-tac-toe/static/css/main.528bf0a5.chunk.css"
+  },
+  {
+    "revision": "1439f38f9ece51aaf058",
+    "url": "/tic-tac-toe/static/js/2.fde396be.chunk.js"
+  },
+  {
+    "revision": "a43f3440d3817e3ad002",
+    "url": "/tic-tac-toe/static/js/main.2bc625c2.chunk.js"
+  },
+  {
+    "revision": "9728fa41a930f12fdc87",
+    "url": "/tic-tac-toe/static/js/runtime~main.f1e29306.js"
+  }
+]);
